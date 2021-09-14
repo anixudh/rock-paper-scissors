629fc1284c6aa7eb4c2252e2609f47a685388a95
anixudh <anirudh.varma196@gmail.com>
2021-09-14 23:46:41 +0530

    added GUI

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,5 @@
 const computerPlay= ()=>{
-    console.log("Hello");
+    //console.log("Hello");
     let choices=["rock","paper","scissors"];
     let pos=parseInt(Math.random()*3);
     return choices[pos];
@@ -41,19 +41,66 @@ const playRPS=(computerSelection, playerSelection)=>{
     }
 }
 
-const game=()=>{
-    for(let i=0;i<5;i++){
-        let playerSelection=prompt("Enter an option- rock/paper/scissors");
-        playerSelection=playerSelection.toLowerCase();
-        let computerSelection=computerPlay();
-        console.log("Computer choice:",computerSelection);
-        console.log(playRPS(computerSelection,playerSelection));
+const checkEnd=()=>{
+    if(cc==5 || pc==5){
+
+        const container=document.querySelector(".container");
+        const resdiv=document.createElement("div");
+        if(cc==5){
+            resdiv.textContent="Max score reached, Computer wins!";
+            resdiv.style.color="red";
+        }
+        else{
+            resdiv.textContent="Max score reached, Player wins!";
+            resdiv.style.color="green";
+        }
+        resdiv.classList.add("result");
+        container.appendChild(resdiv);
+
+        buttons.forEach(button=>button.removeEventListener('click',play));
     }
-}
+};
 
-let pc=0, cc=0;
-game();
-console.log("Final Scores: User: "+pc+" Computer: "+cc);
-if(pc>cc) console.log("You win");
-else console.log("You lose");
+const play= e=>{
 
+    
+    const playerSelection=e.target.getAttribute("class");
+     
+    let computerSelection=computerPlay();
+
+    const res=playRPS(computerSelection, playerSelection);
+
+    const container=document.querySelector(".container");
+
+    const resdiv=document.createElement("div");
+
+    const resdiv1=document.createElement("div"); 
+    resdiv1.textContent=`Computer choice:${computerSelection}`;
+    resdiv1.style.color="red";
+
+    const resdiv2=document.createElement("div"); 
+    resdiv2.textContent=`${res}`;
+    
+    const resdiv3=document.createElement("div"); 
+    resdiv3.textContent=`Computer score: ${cc} Player Score: ${pc}`;
+
+    resdiv.classList.add("resbox");
+
+    resdiv.appendChild(resdiv1);
+    resdiv.appendChild(resdiv2);
+    resdiv.appendChild(resdiv3);
+    container.appendChild(resdiv);
+
+    checkEnd();
+
+};
+
+let cc=0, pc=0;
+
+const buttons=document.querySelectorAll("button");
+buttons.forEach(button=>button.addEventListener('click',play));
+
+/*const element=document.getElementsByClassName("container");
+element.scrollTop = element.scrollHeight*/
+
+element.scrollIntoView(false);
